b315065f0fbaf3431ad48a5436f3b0d566b1287f
sk <sk@sknoiMac.local>
2020-02-25 19:51:22 +0900

    fix julia input

--- Julia/input_ham.jl
+++ Julia/input_ham.jl
@@ -3,22 +3,34 @@ function import_hop(fname,sw_ndegen)
     tmp=open(fname*"/irvec.txt","r") do fp
         readlines(fp)
     end
-    rvec=[float64(split(l)) for l in tmp]
+    tmp2=[split(l) for l in tmp]
+    rvec=[[parse(Float64,l) for l in ll]  for ll in tmp2]
+    nr=length(rvec)
     tmps=open(fname*"/ham_r.txt","r") do fp
         readlines(fp)
     end
-    tmps=[float64(split(strip(l,['\n','(',')']),',')) for l in tmps]
-    ham_r=[complex64(t[1],t[2]) for t in tmps]
-    nr=length(rvec)
-    no=int(sqrt(length(ham_r)/nr))
-    ham_r=reshape(ham_r,no,no,nr)
+    tmp2=[split(strip(l,['\n','(',')']),',') for l in tmps]
+    tmps=[parse(Float64,t[1])+1im*parse(Float64,t[2]) for t in tmp2]
+    no=Int(sqrt(length(tmps)/nr))
+    ham_r=zeros(Complex,nr,no,no)
+    for i in 1:no
+        for j in 1:no
+            for k in 1:nr
+                ham_r[k,j,i]=tmps[k+nr*(j-1)+no*nr*(i-1)]
+            end
+        end
+    end
+
+    for l in ham_r
+        println(l)
+    end
     if sw_ndegen
         tmps=open(fname*"/ndegen.txt","r") do fp
             readlines(fp)
         end
         ndegen=[float64(l) for l in tmp]
     else
-        ndegen=[1 for i in 1:nr]
+        ndegen=ones(Float64,(1,nr))
     end 
     return(rvec,ndegen,ham_r,no,nr)
 end
@@ -27,8 +39,8 @@ function import_out(fname)
     tmp=open(fname,"r") do fp
         readlines(fp)
     end
-    tmp1=[float64(split(l)) for l in tmp]
-    r0=tmp1[1][1:3]
+    tmp1=[split(l) for l in tmp]
+    r0=[parse(Float64,t) for t in tmp1[1][1:3]]
     con=0
     for rr in tmp1
         if rr[1:3]==r0
@@ -37,27 +49,33 @@ function import_out(fname)
     end
     no=int(sqrt(con))
     nr=div(length(tmp1),con)
-    rvec=[tp[1:3] for tp in tmp1[1:nr]]
-    ham_r=reshape([complex64(tp[4],tp[5]) for tp in tmp1],nr,no,no)
-    ndegen=[1 for i in 1:nr]
+    rvec=[[parse(Float64,t) for t in tp[1:3]] for tp in tmp1[1:nr]]
+    ham_r=reshape([parse(Float64,tp[4])+1im*parse(Float64,tp[5]) for tp in tmp1],(nr,no,no))
+    ndegen=ones(Float64,(1,nr))
     return(rvec,ndegen,ham_r,no,nr)
 end
 
-function import_hr(fname):
+function import_hr(fname)
     tmp=open(fname*"_hr.dat","r") do fp
         readlines(fp)
     end
     no,nr=int(tmp[2]),int(tmp[3])
+    return(no,nr)
 end
 
-function import_Hopping():
+function import_Hopping()
     tmp=open("Hopping.dat","r") do fp
         readlines(fp)
     end
-    axis=[float64(split(l)) for l in tmp[2:5]]
-    no,nr,nor=int(split(tmp[5]))
-    tmp1=[float64(split(l)) for l in tmp[7+no:]]
-    rvec=[tmp[no*no*i][:3] for i in 1:nr]
-    ham_r=reshape([complex64(tp[9],tp[10]) for tp in tmp1],nr,no,no)
+    tmp1=[split(l) for l in tmp[2:5]]
+    axis=[[parse(Float64,l) for l in tp] for tp in tmp1]
+    tmp1=split(tmp[5])
+    no=parse(Int,tmp1[1])
+    nr=parse(Int,tmp1[2])
+    nor=parse(Int,tmp1[3])
+    tmp1=[tmp[no*no*i][:3] for i in 1:nr]
+    rvec=[[parse(Float64,l) for l in tp] for tp in tmp1]
+    tmp1=[split(l) for l in tmp[(7+no):end]]
+    ham_r=reshape([parse(Float64,tp[9])+1im*parse(Float64,tp[10]) for tp in tmp1],nr,no,no)
     return(rvec,ndegen,ham_r,no,nr,axis)
 end
